8528c2c515ec0b9a4771b29dadb89614f0780502
Fdg1994 <frederik.de.gottal@gmail.com>
2025-11-03 14:41:14 +0100

    Add mouseover eventlistener to squares that turn them black

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const gridSize = 16;
 const totalSquares = gridSize * gridSize;
 
 for (let i = 0; i < totalSquares; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
-}
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+};
+
+const highlightSquare = (event) => {
+  document.getElementsByClassName("square").style.color = "black";
+};
+square.addEventListener('mousemove', highlightSquare);
+
+
