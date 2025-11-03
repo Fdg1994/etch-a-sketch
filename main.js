ad1534d69bdd19a07f2e5f9578709514ffbab415
Fdg1994 <frederik.de.gottal@gmail.com>
2025-11-03 15:15:47 +0100

    Add createboard function that takes user input for grid size

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 const container = document.querySelector('.container');
+const resizeBtn = document.getElementById('resBtn');
 
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
+function createGrid(size) {
+  
+  container.innerHTML = ''; // clears sketchboard
 
-for (let i = 0; i < totalSquares; i++) {
+  
+  const totalSquares = size * size;
+
+  for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+
+    
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
+
     container.appendChild(square);
-};
+  }
 
-const highlightSquare = (event) => {
-  document.getElementsByClassName("square").style.color = "black";
-};
-square.addEventListener('mousemove', highlightSquare);
+  
+  container.style.setProperty('--grid-size', size);
+}
 
 
+createGrid(16);
+
+
+resBtn.addEventListener('click', () => {
+  let newSize = parseInt(prompt('Enter new grid size (e.g. 16 for 16x16):'));
+  
+  if (Number.isNaN(newSize) || newSize < 1 || newSize > 100) {
+    alert('Please enter a number between 1 and 100.');
+    return;
+  }
+
+  createGrid(newSize);
+});
